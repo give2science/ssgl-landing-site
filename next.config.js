150642ff3111e55d4c1235db3b7cdfd67fcd70c1
nisten <nisten@give2science.com>
2021-11-09 15:54:09 -0500

    telemetry disable

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
-  }
+  },
+  reactStrictMode: true,
+  swcMinify: false,
+  experimental: {
+    concurrentFeatures: true
+  },
 }
