d9ba57946c2e38b604052835e04d18e114c74b6c
nisten <nisten@give2science.com>
2021-11-08 19:38:16 -0500

    node version fix

---
+++ .next/static/h55XFAHHF9OzSeCNANAlg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e36e9ca00e00d53e.css","static/chunks/pages/index-30b81784757833a4.js"],"/_error":["static/chunks/pages/_error-f264e1bec326820a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
