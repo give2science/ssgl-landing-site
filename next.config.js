b9617177973dc9f347e5eb5cdee7d6ca00ba0e9c
nisten <nisten@give2science.com>
2021-11-09 15:49:52 -0500

    fixes 3

--- next.config.js
+++ next.config.js
@@ -2,10 +2,5 @@ module.exports = {
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    concurrentFeatures: true
-  },
+  }
 }
